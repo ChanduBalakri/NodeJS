f262c0bcc1b3d6ea5fd44dd2f350e80943abe89f
Chandresh <chandub2602@gmail.com>
2023-02-02 18:23:15 -0500

    adding files...

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
-const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const app = express();
 
-console.log(uuidv4());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 3000;
+
+const customers = [
+    {
+        "name": "Chandresh",
+        "industry": "music"
+    },
+    {
+        "name": "Ashni",
+        "industry": "networking"
+    },
+    {
+        "name": "Jayanthy",
+        "industry": "sports medicine"
+    },
+];
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+});
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers});
+});
+
+app.post('/api/customers', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.post('/', (req, res) => {
+    res.send('This is a post request!');
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT);
+});
